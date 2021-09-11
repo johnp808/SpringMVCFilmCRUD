06239ff4619634bf7625859caf3ad7de2e00a872
Me <johnpuliatch@gmail.com>
2021-09-10 14:48:08 -1000

    config spring

---
+++ src/main/java/com/skilldistillery/film/dao/FilmDAO.java
@@ -0,0 +1,10 @@
+package com.skilldistillery.film.dao;
+
+import com.skilldistillery.film.entities.Film;
+
+public interface FilmDAO {
+
+	Film findById(int filmId);
+	
+	
+}
---
+++ src/main/java/com/skilldistillery/film/dao/FilmDaoJdbcImpl.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.film.dao;
+
+import com.skilldistillery.film.entities.Film;
+
+public class FilmDaoJdbcImpl implements FilmDAO {
+
+	@Override
+	public Film findById(int filmId) {
+		
+		return null;
+	}
+}
